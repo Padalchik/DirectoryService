8b4dfad76c61dbf63c40afb7b6471475764a3109
Padalchik <andrey_nikiforov_1998@mail.ru>
2025-08-28 23:14:56 +0300

    done DS-B-8

---
+++ DirectoryService/src/DirectoryService.Presentation/Middlewares/ExeptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using DirectoryService.Domain.Shared;
+using DirectoryService.Presentation.Response;
+
+namespace DirectoryService.Presentation.Middlewares;
+
+public class ExeptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExeptionMiddleware> _logger;
+
+    public ExeptionMiddleware(RequestDelegate next, ILogger<ExeptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogInformation(ex.Message);
+
+            var error = Error.Failure("internal.server.exception", ex.Message);
+            var envelope = Envelope.Error(error);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            await context.Response.WriteAsJsonAsync(envelope);
+        }
+    }
+}
+
+public static class ExeptionMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCustomExeptionMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExeptionMiddleware>();
+    }
+}
