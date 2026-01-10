44d7c0880517f311f4a487db28288ebee7092237
Никифоров Андрей <andrey_nikiforov_1998@mail.ru>
2026-01-10 17:42:20 +0300

    Moved to extensions methods

---
+++ DirectoryService/src/DirectoryService.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using DirectoryService.Application.Abstractions;
+using DirectoryService.Application.Locations.CreateLocation;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DirectoryService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(CreateLocationHandler).Assembly;
+
+        services.Scan(scan => scan
+            .FromAssemblies(assembly)
+            .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<,>)))
+            .AsSelfWithInterfaces()
+            .WithScopedLifetime());
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
